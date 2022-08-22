77f8a573138b97b745925b366e4a4cf70f78bf6f
tys897897 <2709522881@qq.com>
2022-08-22 21:41:36 +0800

    feature --  双向链表

--- 05-双向链表.js
+++ 05-双向链表.js
@@ -22,6 +22,33 @@ function DoublyLinkedList() {
     this.length++;
   };
 
+  DoublyLinkedList.prototype.insert = (position, data) => {
+    // 越界判断
+    if (position < 0 || position > this.length) return false;
+    var node = new Node(data);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else if (position === 0) {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    } else if (position === this.length) {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      var current = this.head;
+      for (var i = 0; i < position; i++) {
+        current = current.next;
+      }
+      node.prev = current.prev;
+      node.next = current;
+      current.prev.next = node;
+      current.prev = node;
+    }
+  };
+
   DoublyLinkedList.prototype.String = () => {
     return this.backwardString();
   };
